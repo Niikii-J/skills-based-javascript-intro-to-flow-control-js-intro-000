c390dce80fee1eaa6a33792d7fe02608ede0679a
Nikita Johnson <nikita_johnson@hotmail.com>
2020-10-16 17:57:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -49,6 +49,14 @@ function switchAge(age) {
  return "You are a teenager";
  break;
 
+ case 18:
+ return "You are a teenager";
+ break;
+
+ case 19:
+ return "You are a teenager";
+ break;
+
  default:
  return: "You have an age";
 }
