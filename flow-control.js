dde255ad52040487cd27f5030a49b16fb8405f9a
Nikita Johnson <nikita_johnson@hotmail.com>
2020-10-16 17:48:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -23,8 +23,8 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-return 13 <= age && age <= 19 ? 
-"You are a teenager" : 
+return 13 <= age && age <= 19 ?
+"You are a teenager" :
 "you are not a teenager"
 }
 
